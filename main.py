14dfffe66af32884545eb476ad0ff342c7926923
Replit user <>
2024-02-08 21:50:12 +0000

    Add test route

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+
+@app.get("/items/")
+async def get_items():
+  return {"greeting": "Hello"}
